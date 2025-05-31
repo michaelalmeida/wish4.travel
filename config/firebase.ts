c820357c5a8f4c77101fce67d08144f8b4258aa6
Michael <michaelralmeida@gmail.com>
2025-05-31 20:43:03 +0200

    fix: add appcheck

--- config/firebase.ts
+++ config/firebase.ts
@@ -1,4 +1,5 @@
 import { initializeApp } from "firebase/app";
+import { initializeAppCheck, ReCaptchaV3Provider } from "firebase/app-check";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_CLIENT_KEY,
@@ -7,8 +8,12 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_CLIENT_STORAGE,
   messagingSenderId: import.meta.env.VITE_FIREBASE_CLIENT_MESSAGE,
   appId: import.meta.env.VITE_FIREBASE_CLIENT_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_CLIENT_MEANSUREMENT,
+  measurementId: import.meta.env.VITE_FIREBASE_CLIENT_MEASUREMENT,
 };
 
-// Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+
+export const appCheck = initializeAppCheck(app, {
+  provider: new ReCaptchaV3Provider(import.meta.env.VITE_RECAPTCHA_SITE_KEY),
+  isTokenAutoRefreshEnabled: true,
+});
